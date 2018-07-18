5adc1cd95597f9b21d7cd37750f15513befffa93
ilyana19 <aries_2152@hotmail.com>
2018-07-17 23:22:05 -0400

    simplified ex8

--- ex8.rb
+++ ex8.rb
@@ -6,47 +6,40 @@ def calc_speed(dist, time)
   return dist / time
 end
 
+# previously, return 2 values and i think that's bad practice and then remembered about calling other methods inside of a method...
 def record(dist, min)
-  secs = min_to_sec(min)
-  speed = calc_speed(dist, secs)
-  return secs, speed
+  speed = calc_speed(dist, min_to_sec(min))
+  return speed
 end
 
 #compare records
-def compare_records(p1, p2, p3)
-  if p3[1] > p2[1] && p3[1] > p1[1]
-    return "Person 3 was the fastest at #{p3[1]} m/s."
-  elsif p2[1] > p3[1] && p2[1] > p1[1]
-    return "Person 2 was the fastest at #{p3[1]} m/s."
-  elsif p1[1] > p3[1] && p1[1] > p2[1]
-    return "Person 1 was the fastest at #{p1[1]} m/s"
-  elsif p1[1] == p2[1] && p2[1] == p3[1]
-    return "Everyone tied at #{p1[1]} m/s"
+def compare_records(speed1, speed2, speed3)
+  if speed3 > speed2 && speed3 > speed1
+    puts "Person 3 was the fastest at #{speed3} m/s"
+  elsif speed2 > speed3 && speed2 > speed1
+    puts "Person 2 was the fastest at #{speed2} m/s"
+  elsif speed1 > speed3 && speed1 > speed2
+    puts "Person 1 was the fastest at #{speed1} m/s"
+  elsif speed1 == speed2 && speed2 == speed3
+    puts "Everyone tied at #{speed1} m/s"
   else
-    return "Well done everyone!"
+    puts "Well done everyone!"
   end
 end
 
 # get info
-puts "How far did person 1 run (in metres)?"
-distance1 = gets.to_f
-puts "How long (in minutes) did person 1 run take to run #{distance1} metres?"
-mins1 = gets.to_f
+def get_info(person)
+  puts "How far did person #{person} run (in metres)?"
+  distance = gets.to_f
+  puts "How long (in minutes) did person #{person} run take to run #{distance} metres?"
+  time = gets.to_f
+  speed = record(distance, time) #calculate their speed
+end
 
-puts "How far did person 2 run (in metres)?"
-distance2 = gets.to_f
-puts "How long (in minutes) did person 2 take to run #{distance2} metres?"
-mins2 = gets.to_f
-
-puts "How far did person 3 run (in metres)?"
-distance3 = gets.to_f
-puts "How long (in minutes) did person 3 take to run #{distance3} metres?"
-mins3 = gets.to_f
-
-# do calculations
-person1 = record(distance1, mins1)
-person2 = record(distance2, mins2)
-person3 = record(distance3, mins3)
+# call the get_info method to collect data
+person1 = get_info(1)
+person2 = get_info(2)
+person3 = get_info(3)
 
 # show results
 puts compare_records(person1, person2, person3)
